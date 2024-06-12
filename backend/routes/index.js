d74598847e75a1b922fb76e450f04ee415e95d09
Muhamad Krisnandi <mkrisnandi08@gmail.com>
2024-06-12 08:12:39 +0700

    feature crud users

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const { validateRegister, validateLogin } = require("../utils/validators/auth");
+const { validateUser } = require("../utils/validators/user");
+const verifyToken = require("../middlewares/auth");
 const { register } = require("../controllers/RegisterController");
 const { login } = require("../controllers/LoginController");
+const {
+  findUsers,
+  createUser,
+  findUserById,
+  updateUser,
+  deleteUser,
+} = require("../controllers/UserController");
 const router = express.Router();
 
 router.post("/register", validateRegister, register);
 router.post("/login", validateLogin, login);
+router.get("/admin/users", verifyToken, findUsers);
+router.post("/admin/users", verifyToken, validateUser, createUser);
+router.get("/admin/users/:id", verifyToken, findUserById);
+router.put("/admin/users/:id", verifyToken, validateUser, updateUser);
+router.delete("/admin/users/:id", verifyToken, validateUser, deleteUser);
 
 module.exports = router;
